7cfaf0ccd9f52953325b95085c78c8f590e50c73
Emperatrizada <166084550+Emperatrizada@users.noreply.github.com>
2024-05-20 22:30:57 -0600

    Add files via upload

---
+++ Introduccion a la progra 04 tarea/tarea 4/problema1.js
@@ -0,0 +1,11 @@
+letrespuesta = 'Brendan Eich';
+
+letrespuesta = prompt (Ingresa una respuesta);
+
+
+if( respuesta === 'Brendan Eich') {
+    console.log(" ¡Correcto! ");
+}
+else {console.log("¿No lo sabes? ¡Brendan Eich!"
+)
+;}
