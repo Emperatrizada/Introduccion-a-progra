bcd002b1c3a8c62198a36ea40fe02175cbaf9fe6
Emperatrizada <166084550+Emperatrizada@users.noreply.github.com>
2024-05-22 23:49:00 -0600

    Add files via upload

---
+++ Introduccion a la progra 04 tarea/tarea 4/Introduccion a la progra 04 tarea/tarea 4/problema4.js
@@ -0,0 +1,25 @@
+let username = 'Admin';
+username = prompt("Ingresa nombre de usuario");
+
+if(username == 'Admin') {
+    contraseña = prompt("Ingresa contraseña");
+
+let contraseña ='Jedi';
+
+if (contraseña === 'Jedi') {
+    console.log('¡Que la fuerza te acompañe!');
+}
+else {
+    console.log('Contraseña incorrecta');
+}
+if (contraseña === "") {
+    console.log('Cancelado');
+}
+}
+else if (username == escape){
+    console.log('Cancelado');
+}
+else{
+    console.log('No te conozco');}
+
+   
